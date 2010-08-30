5c42dbce952b0a1acccb4e5b3a1a3db23524d038
Adam Soltys <adam.soltys@pwgsc-tpsgc.gc.ca>
2010-08-30 16:51:06 -0600

    Display report totals and also output non-billable time (refs #3971)

--- lib/time_entry_patch.rb
+++ lib/time_entry_patch.rb
@@ -5,6 +5,10 @@ module TimesheetTimeEntryPatch
     base.class_eval do
       unloadable # Send unloadable so it will not be unloaded in development
       named_scope :by_user, lambda { |user_id| { :conditions => ["user_id = ?", user_id]}}
+
+      def value
+        return (hours/7.5) * 445
+      end
     end
   end
 end

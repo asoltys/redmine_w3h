683b7067e78fe9a5e4c8cf63d21669df2fbb54e5
Adam Soltys <adam.soltys@pwgsc-tpsgc.gc.ca>
2010-08-31 04:51:01 -0600

    Got deliverables toggle working (refs #3971)

--- assets/javascripts/timesheet.js
+++ assets/javascripts/timesheet.js
@@ -1,8 +1,14 @@
 Event.observe(window, 'load', function() { 
   $$('tr.deliverable').each(function(e) { 
     e.observe('click', function(e) {
-      e.element().up('table').select('tr.activity').each(function(e) {
-        e.toggle();
+      e.element().up('tbody').nextSiblings().each(function(e) {
+        if (e.classNames().include('deliverable_end')) {
+          throw $break;
+        }
+
+        if (!e.classNames().include('time_entries')) {
+          e.toggle();
+        }
       });
     });
   });

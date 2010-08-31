0e03ae206d8194dbb5aa4cbd938d1f386f216d7d
Adam Soltys <adam.soltys@pwgsc-tpsgc.gc.ca>
2010-08-31 12:10:25 -0600

    Working out some glitches (refs #3971)

--- assets/javascripts/timesheet.js
+++ assets/javascripts/timesheet.js
@@ -1,14 +1,29 @@
 Event.observe(window, 'load', function() { 
+  $$('tr.snowcone').each(function(e) { 
+    e.observe('click', function(e) {
+      var row = e.element();
+      row.classNames().include('hiding') ? row.removeClassName('hiding') : row.addClassName('hiding');
+      row.up('tbody').nextSiblings().each(function(e) {
+        if (e.classNames().include('user_end')) {
+          throw $break;
+        }
+
+        row.classNames().include('hiding') ? e.hide() : e.show();
+      });
+    });
+  });
+
+
   $$('tr.deliverable').each(function(e) { 
     e.observe('click', function(e) {
-      e.element().up('tbody').nextSiblings().each(function(e) {
+      var row = e.element();
+      row.classNames().include('hiding') ? row.removeClassName('hiding') : row.addClassName('hiding');
+      row.up('tbody').nextSiblings().each(function(e) {
         if (e.classNames().include('deliverable_end')) {
           throw $break;
         }
 
-        if (!e.classNames().include('time_entries')) {
-          e.toggle();
-        }
+        row.classNames().include('hiding') ? e.hide() : e.show();
       });
     });
   });

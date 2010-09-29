6cee542382c914bebe753401b7d88838bb117a99
Adam Soltys <adam.soltys@pwgsc-tpsgc.gc.ca>
2010-09-29 15:10:38 -0600

    custom time entry form

--- lib/user_patch.rb
+++ lib/user_patch.rb
@@ -4,7 +4,7 @@ module TimesheetUserPatch
   def self.included(base) # :nodoc:
     base.class_eval do
       unloadable # Send unloadable so it will not be unloaded in development
-      named_scope :active, :conditions => ['status = ?', User::STATUS_ACTIVE]
+      named_scope :active, :conditions => ['status = ?', User::STATUS_ACTIVE], :order => 'firstname'
       named_scope :time_recorders, :conditions => "users.id IN (SELECT DISTINCT user_id FROM time_entries)"
       has_many :time_entries, :dependent => :delete_all
 

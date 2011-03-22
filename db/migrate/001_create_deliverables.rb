8043cb93cbb5a54b36d6b753a90eb7c768d61601
Adam Soltys <adam.soltys@pwgsc-tpsgc.gc.ca>
2011-03-21 23:31:10 -0600

    add a check to prevent migrations from being run if the equivalent budget/timesheet migrations have been run

--- db/migrate/001_create_deliverables.rb
+++ db/migrate/001_create_deliverables.rb
@@ -1,13 +1,27 @@
 class CreateDeliverables < ActiveRecord::Migration
+  class SchemaMigration < ActiveRecord::Base
+  end
+
   def self.up
-    create_table :deliverables do |t|
-      t.column :subject, :string
-      t.column :due_date, :date
-      t.column :description, :text
-      t.column :type, :string
-      t.column :cost, :decimal, :precision => 15, :scale => 2
-      t.column :project_manager_signoff, :boolean, :default => false
-      t.column :client_signoff, :boolean, :default => false
+    # Pretend like we've run the migrations in the w3h plugin if we've already run them for the old budget and timesheet plugins
+    ['budget_plugin', 'timesheet_plugin'].each do |legacy_plugin|
+      SchemaMigration.find(:all, :conditions => "version LIKE '%#{legacy_plugin.gsub('_', '\\_')}%'").each do |migration|
+        unless migration.version == '1-budget_plugin'
+          SchemaMigration.create(:version => migration.version.gsub(legacy_plugin, 'redmine_w3h')) 
+        end
+      end
+    end
+
+    unless SchemaMigration.find(:all, :conditions => "version = '1-budget_plugin'")
+      create_table :deliverables do |t|
+        t.column :subject, :string
+        t.column :due_date, :date
+        t.column :description, :text
+        t.column :type, :string
+        t.column :cost, :decimal, :precision => 15, :scale => 2
+        t.column :project_manager_signoff, :boolean, :default => false
+        t.column :client_signoff, :boolean, :default => false
+      end
     end
   end
   

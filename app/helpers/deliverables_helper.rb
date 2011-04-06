95bb713823fbe02a00e33de7276e3316efcd76c5
Justin Quach <justin.quach@pwgsc-tpsgc.gc.ca>
2011-04-06 11:44:10 -0700

    Cleaning Magic Numbers

--- app/helpers/deliverables_helper.rb
+++ app/helpers/deliverables_helper.rb
@@ -99,23 +99,4 @@ module DeliverablesHelper
     return number_to_percentage(percent_field, :precision => 0) unless percent_field.blank?
     return "$0"
   end
-	
-	def render_custom_fields_rows(deliverable)
-    return if deliverable.custom_field_values.empty?
-    ordered_values = []
-    half = (deliverable.custom_field_values.size / 2.0).ceil
-    half.times do |i|
-      ordered_values << deliverable.custom_field_values[i]
-      ordered_values << deliverable.custom_field_values[i + half]
-    end
-    s = "<tr>\n"
-    n = 0
-    ordered_values.compact.each do |value|
-      s << "</tr>\n<tr>\n" if n > 0 && (n % 2) == 0
-      s << "\t<th>#{ h(value.custom_field.name) }:</th><td>#{ simple_format_without_paragraph(h(show_value(value))) }</td>\n"
-      n += 1
-    end
-    s << "</tr>\n"
-    s
-  end
 end

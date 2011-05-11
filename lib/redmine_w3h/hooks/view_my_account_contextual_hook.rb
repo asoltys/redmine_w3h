854da1fd85730cf083d662198d9366b5fe8ca7ae
Adam Soltys <adam.soltys@pwgsc-tpsgc.gc.ca>
2011-05-11 15:52:56 -0600

    fixed hook to append link_to output rather than only outputting the last one listed

--- lib/redmine_w3h/hooks/view_my_account_contextual_hook.rb
+++ lib/redmine_w3h/hooks/view_my_account_contextual_hook.rb
@@ -1,6 +1,6 @@
 class ViewMyAccountContextualHook < Redmine::Hook::ViewListener
   def view_my_account_contextual(context = {})
-    link_to 'Time Recording Settings', :controller => 'timesheet', :action => 'settings'
-    link_to 'My Profile', :controller => 'users', :action => 'show', :id => User.current
+    l = link_to 'Time Recording Settings', :controller => 'timesheet', :action => 'settings'
+    l << link_to 'My Profile', :controller => 'users', :action => 'show', :id => User.current
   end
 end

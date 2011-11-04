73edebaf7b6fcd5532c0faf72598985445be0a40
Adam Soltys <adam.soltys@pwgsc-tpsgc.gc.ca>
2011-11-04 01:18:23 -0600

    Assimilate Eric Davis' bulk timesheet plugin

--- lib/redmine_w3h/patches/time_entry_patch.rb
+++ lib/redmine_w3h/patches/time_entry_patch.rb
@@ -27,6 +27,17 @@ module RedmineW3H
           def overtime_hours
             hours - regular_hours
           end
+
+          def create_bulk_time_entry(entry)
+            time_entry = TimeEntry.new(entry)
+            time_entry.hours = nil if time_entry.hours.blank? or time_entry.hours <= 0
+            if BulkTimeEntriesController.allowed_project?(entry[:project_id])
+              time_entry.project_id = entry[:project_id] # project_id is protected from mass assignment
+            end
+            time_entry.user = User.current
+            time_entry.save
+            time_entry
+          end
         end
       end
     end
